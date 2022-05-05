8e0fe39a8d66c2428aa1154ce7c6c58b656947f2
TenshiMunasinghe <shadowghost1007@gmail.com>
2022-05-05 12:48:09 +0900

    adjust grid size

--- src/components/Day.vue
+++ src/components/Day.vue
@@ -10,7 +10,7 @@
   <li class="flex flex-col space-y-3">
     <h3 class="text-lg">{{ label.replaceAll('-', '/') }}</h3>
     <ul
-      class="grid grid-cols-4 md:grid-cols-[repeat(auto-fit,7rem)] gap-3 lg:gap-6"
+      class="grid grid-cols-4 md:grid-cols-[repeat(auto-fit,6rem)] gap-3 lg:gap-6"
     >
       <Weather v-for="weather in weathers" :weather="weather" />
     </ul>
