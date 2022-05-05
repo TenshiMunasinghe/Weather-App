25184ddee869dab888779eb4d795a8e9ba9ed74a
TenshiMunasinghe <shadowghost1007@gmail.com>
2022-05-05 12:23:24 +0900

    extract weather component

---
+++ src/components/Weather.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import { kelvinsToCelsius } from '../utils/kelvinsToCelsius'
+  import WeatherIcon from './WeatherIcon.vue'
+  defineProps<{ weather: any }>()
+</script>
+
+<template>
+  <li
+    class="bg-blue-900/50 rounded-sm py-4 px-6 whitespace-nowrap flex flex-col space-y-2 justify-center items-center"
+  >
+    <h4>{{ weather.time.substr(0, 5) }}</h4>
+    <WeatherIcon class="w-9 h-9" :icon-id="weather.weather[0].icon" />
+    <div v-if="typeof weather.main.temp === 'number'">
+      {{ kelvinsToCelsius(weather.main.temp) }}&#x2103;
+    </div>
+  </li>
+</template>
